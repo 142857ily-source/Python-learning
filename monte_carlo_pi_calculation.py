a840ae94363f74fa7e641efbffe954bc24c629f4
Qijun Lin <142857ily@gmail.com>
2026-09-07 14:06:59 -0500

    Add files via upload

---
+++ monte_carlo_pi_calculation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+n=int(input(f'Enter your input:'))
+x=np.random.random(n)
+y=np.random.random(n)
+
+inside = x**2 + y**2 < 1
+plt.scatter(x[inside], y[inside], s=2)
+plt.scatter(x[~inside], y[~inside], s=2)
+theta = np.linspace(0, np.pi/2, 100)
+plt.plot(np.cos(theta), np.sin(theta))
+plt.axis("equal")
+plt.show()
+
+def monte_carlo(x,y):
+    count=0
+    for i,j in zip(x,y):
+        if (i**2 + j**2)<1:
+            count+=1
+    return count
+count=monte_carlo(x,y)
+pi=4*count/n
+print (f"Monte_carlo_pi={pi}")
+print (f'Error={abs(100*(pi - math.pi)/math.pi)}%')
+
+
+
+
+
