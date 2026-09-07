60c7a4d28fa5e73bf61e62f5598558ee0339853a
Qijun Lin <142857ily@gmail.com>
2026-09-07 13:05:30 -0500

    Update 1D_random_walk.py

--- 1D_random_walk.py
+++ 1D_random_walk.py
@@ -8,16 +8,19 @@ def random_walk(steps):
         step = int(np.random.choice([-1, 1]))
         position = position + step
         positions.append(position)
+    k_list=[0]
     for k in range (1,steps):
         if positions[k]==0:
-            print (k)
-    distance=(abs((positions[-1])))
-    print (distance)
+            k_list.append(k)
+    print (k_list)
+    displacement=(abs((positions[-1])))
+    print (f"displacement={displacement}")
     return positions 
 steps = int(input ("Enter your steps:",))
 walk = random_walk(steps)
 plt.plot(walk)
 plt.show()
+
 
 
 
